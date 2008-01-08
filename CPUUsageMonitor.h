466ca6fea0ecbfd5f1cdd2a8995e52ea910502d3
Christopher Bowns <christopher@cbowns.com>
2008-01-08 18:35:49 -0500

    adding cpuusage code from hozey. heavily edited. haven't compiled yet.

--- CPUUsageMonitor.h
+++ CPUUsageMonitor.h
@@ -12,66 +12,24 @@
 #include <mach/processor_info.h>
 #include <mach/mach_host.h>
 
-enum CPUUsageOrientation {
-	CPUUsageOrientationVertical,
-	CPUUsageOrientationHorizontal,
-	CPUUsageOrientationRectangular
-};
+typedef struct cpudata {
+	double user;
+	double sys;
+	double nice;
+	double idle;
+} CPUData, CPUDataPtr*;
 
 @interface CPUUsageMonitor : NSObject {
-	NSWindow *window;
-	NSPanel *dockIconWindow; //Dock icon view is put in here.
-
-	//Preferences views.
-	IBOutlet NSPanel *preferencesPanel;
-	NSColor *backgroundColor;
-	float cellWidth, cellHeight;
-	IBOutlet NSTextField *widthField, *heightField;
-	IBOutlet NSButton *horizontalButton, *verticalButton, *rectangularButton;
-
-	IBOutlet NSMenu *contextualMenu;
-
-	NSMutableArray *usageViews, *dockIconUsageViews;
-	NSView *dockIconUsageViewsContainingView;
-	enum CPUUsageOrientation orientation;
-	BOOL shouldDrawToDockIcon, shouldDrawToWindow;
-
-	NSTimer *updateTimer;
 	processor_info_array_t lastProcessorInfo;
 	mach_msg_type_number_t numLastProcessorInfo;
 	unsigned numCPUs;
 	float *CPUUsage;
 	NSLock *CPUUsageLock, *deathLock;
-	unsigned threadsRemainingToDie;
-	BOOL threadsShouldExit;
-
-	//Saved preferences, for restoration after Cancel.
-	NSColor *savedBackgroundColor;
-	NSSize   savedCellSize;
-	enum CPUUsageOrientation savedOrientation;
+	CPUDataPtr		cpudata;
 }
 
-#pragma mark Accessors
+- (cpudata)updateCPUUsage;
 
-- (NSColor *)backgroundColor;
-- (void)setBackgroundColor:(NSColor *)newBackgroundColor;
 
-- (float)cellWidth;
-- (void)setCellWidth:(float)newCellWidth;
-
-- (float)cellHeight;
-- (void)setCellHeight:(float)newCellHeight;
-
-- (BOOL)shouldDrawToDockIcon;
-- (void)setShouldDrawToDockIcon:(BOOL)flag;
-
-- (BOOL)shouldDrawToWindow;
-- (void)setShouldDrawToWindow:(BOOL)flag;
-
-#pragma mark Actions
-
-- (IBAction)setOrientationToVertical:sender;
-- (IBAction)setOrientationToHorizontal:sender;
-- (IBAction)setOrientationToRectangular:sender;
 
 @end
